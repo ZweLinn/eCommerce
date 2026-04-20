ca9d0e7df62ab9ba7ba2b0ac87b2a5b2b7f86ad8
zwelinn <zewlinnmg@gmail.com>
2026-04-20 17:16:27 +0700

    Add admin Template

--- resources/views/user/home/list.blade.php
+++ resources/views/user/home/list.blade.php
@@ -1,6 +1,13 @@
 @extends('user.layouts.master')
 
 @section('content')
+    <div >
     <h1>Fruitables - Vegetable Website Template</h1>
     <p>Welcome to our vegetable website!</p>
+    <p>Welcome to our vegetable website!</p>
+    <p>Welcome to our vegetable website!</p>
+    <p>Welcome to our vegetable website!</p>
+    <p>Welcome to our vegetable website!</p>
+    <p>Welcome to our vegetable website!</p>
+    </div>
 @endsection
--- routes/user.php
+++ routes/user.php
@@ -1,3 +1,8 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/home', [UserController::class, 'userHome'])->name('userHome');
+});
